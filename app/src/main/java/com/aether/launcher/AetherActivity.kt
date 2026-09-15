2f0b592bfea6a9b18c2c838f2c40164dce82a0ca
benjaminchume-droid <benjaminchume@gmail.com>
2026-09-15 18:47:28 +0100

    Enable true edge-to-edge Aether launch surface

--- app/src/main/java/com/aether/launcher/AetherActivity.kt
+++ app/src/main/java/com/aether/launcher/AetherActivity.kt
@@ -2,6 +2,7 @@ package com.aether.launcher
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.core.view.WindowCompat
 import com.aether.launcher.settings.AetherSettingsStore
 import com.aether.launcher.settings.AetherSetupView
 import com.aether.launcher.ui.AetherGlassRoot
@@ -9,12 +10,19 @@ import com.aether.launcher.ui.AetherGlassRoot
 class AetherActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.enableEdgeToEdge(window)
         AetherRuntime.initialize(applicationContext)
         val store=AetherSettingsStore(this)
         if(store.load().setupComplete){
-            val glass=AetherGlassRoot(this);glass.attach(AetherHomeView(this));setContentView(glass)
+            val glass=AetherGlassRoot(this)
+            glass.attach(AetherHomeView(this))
+            setContentView(glass)
         }else{
-            setContentView(AetherSetupView(this){val glass=AetherGlassRoot(this);glass.attach(AetherHomeView(this));setContentView(glass)})
+            setContentView(AetherSetupView(this){
+                val glass=AetherGlassRoot(this)
+                glass.attach(AetherHomeView(this))
+                setContentView(glass)
+            })
         }
     }
 }
